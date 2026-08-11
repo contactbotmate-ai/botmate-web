8b3a66dfa282d67ca520229e6891dd5a308ce73a
Pallabi kar <kpallabi918@gmail.com>
2026-08-11 12:51:49 +0530

    Fix async handler declaration in GetStartedModal

--- frontend/components/GetStartedModal.tsx
+++ frontend/components/GetStartedModal.tsx
@@ -162,7 +162,7 @@ export default function GetStartedModal() {
     setFormData({ ...formData, interest: opt });
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     
     // Mark all as touched
